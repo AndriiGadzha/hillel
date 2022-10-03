c5134c4649c8dd6f538dae64eb218026eacb726a
Andrej <gab0959248699@gmail.com>
2022-10-04 01:46:12 +0300

    dz7

--- dz7/index.js
+++ dz7/index.js
@@ -1,1 +1,34 @@
 // -Вывести на страницу в одну строку через запятую числа от 10 до 20
+// let str = "";
+// for (let i = 10; i <= 20; i++) {
+//   str += ", " + i;
+// }
+// // console.log(str.substring(1));
+// console.log(str.slice(-42));
+
+// -Вывести квадраты чисел от 10 до 20
+// let str = "";
+// for (let i = 10; i <= 20; i++) {
+//   str += ", " + i * i;
+// }
+// console.log(str.substring(1));
+
+// -Вывести таблицу умножения на 7
+// let str = "";
+// let num = 7;
+// for (let i = 1; i <= 9; i++) {
+//   str += `${num}*${i}=${num * i} `;
+// }
+// console.log(str);
+
+// -Найти сумму всех целых чисел от 1 до 15
+// let num = 15
+// let sum = num*(num+1)/2
+// console.log(sum);
+
+// function sumNum(n) { 
+//     let sum = 0; 
+//     for (let i = 1; i <= n; i++) { 
+//         sum += i; 
+//     } return sum; 
+// } console.log(sumNum(15)); 
