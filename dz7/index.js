a5ed390995591090680213092f6791ca6e229d14
Andrej <gab0959248699@gmail.com>
2022-10-04 17:35:04 +0300

    dz7

--- dz7/index.js
+++ dz7/index.js
@@ -13,22 +13,48 @@
 // }
 // console.log(str.substring(1));
 
-// -Вывести таблицу умножения на 7
-// let str = "";
-// let num = 7;
-// for (let i = 1; i <= 9; i++) {
-//   str += `${num}*${i}=${num * i} `;
+// for (let i = 10; i <= 20; i++) {
+//   console.log(i*i);
 // }
-// console.log(str);
+
+// -Вывести таблицу умножения на 7
+// let num = 7;
+// let result;
+// for (let i = 1; i <= 9; i++) {
+//     result = `${num}*${i}=${num * i}`
+//   console.log(result);
+// }
+
 
 // -Найти сумму всех целых чисел от 1 до 15
 // let num = 15
 // let sum = num*(num+1)/2
 // console.log(sum);
 
-// function sumNum(n) { 
-//     let sum = 0; 
-//     for (let i = 1; i <= n; i++) { 
-//         sum += i; 
-//     } return sum; 
-// } console.log(sumNum(15)); 
+// function sumNum(n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         sum += i;
+//     } return sum;
+// } console.log(sumNum(15));
+
+
+
+// -Найти произведение всех целых чисел от 15 до 35
+// let res = 1;
+// for (let i = 15; i <= 35; i++) {
+//  res *= i;
+// }
+// console.log(res);
+
+// let i = 15;
+// let sum = 1;
+// while (i <= 35) {  
+//   sum *= i;
+//   i++;
+// }
+// console.log(sum);
+
+
+
+// -Найти среднее арифметическое всех целых чисел от 1 до 500
